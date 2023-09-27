d979da64c3db9726922e86452ca7e5824f2505af
Ruben <rubend.alvarezc@gmail.com>
2023-09-26 20:39:32 -0400

    include multiple forms

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { prompt } = require('enquirer');
 const fs = require('fs');
-const SVGBuilder = require('svg-builder');
 
 function getUserInput() {
   return prompt([
@@ -14,35 +13,45 @@ function getUserInput() {
       name: 'letters',
       message: 'Enter up to 3 letters to include in the SVG:',
       validate: input => input.length <= 3 || 'Please enter a maximum of 3 letters.'
+    },
+    {
+      type: 'select',
+      name: 'shape',
+      message: 'Choose a shape for the SVG:',
+      choices: ['circle', 'triangle', 'rectangle']
     }
   ]);
 }
 
-function generateSVG(color, letters) {
-  const svg = new SVGBuilder();
-  const width = 100;
-  const height = 100;
+function generateSVGString(color, letters, shape) {
+  let svgContent;
 
-  svg.width(width).height(height);
-  svg.rect(width, height).fill(color);
+  if (shape === 'circle') {
+    svgContent = `<circle cx="50" cy="50" r="40" fill="${color}" />`;
+  } else if (shape === 'triangle') {
+    svgContent = `<polygon points="50,10 90,90 10,90" fill="${color}" />`;
+  } else {
+    svgContent = `<rect width="100" height="100" fill="${color}" />`;
+  }
 
-  const textX = width / 2;
-  const textY = height / 2;
-  svg.text(letters).move(textX, textY).fill('#FFFFFF');
-
-  return svg;
+  return `<svg width="100" height="100">
+    ${svgContent}
+    <text x="50%" y="50%" fill="#ffffff" text-anchor="middle" dy=".3em">${letters}</text>
+  </svg>`;
 }
 
-function createSVGFile(svg, filePath) {
-  fs.writeFileSync(filePath, svg.toString());
+function createSVGFile(svgString, filePath) {
+  fs.writeFileSync(filePath, svgString);
   console.log(`SVG file saved at: ${filePath}`);
 }
 
 async function main() {
   const userInput = await getUserInput();
   const { color, letters } = userInput;
-  const svg = generateSVG(color, letters);
-  createSVGFile(svg, 'logo.svg');
+  const svgString = generateSVGString(color, letters);
+  createSVGFile(svgString, 'logo.svg');
 }
 
 main();
+
+
