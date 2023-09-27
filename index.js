becf8290eb6b90230f40c409da067dd4dea839c5
Ruben <rubend.alvarezc@gmail.com>
2023-09-26 22:33:46 -0400

    latest changes

--- index.js
+++ index.js
@@ -4,50 +4,52 @@ const fs = require('fs');
 function getUserInput() {
   return prompt([
     {
-      type: 'input',
+      type: 'select',
+      name: 'shape',
+      message: 'Choose a shape for the SVG:',
+      choices: ['circle','triangle','rectangle']
+    },
+    {
+      type: 'select',
       name: 'color',
-      message: 'Enter a color for the SVG (e.g., #FF5733):'
+      message: 'Enter a color for the SVG:',
+      choices: ['red','blue', 'green', 'yellow','gray','orange']
     },
     {
       type: 'input',
       name: 'letters',
       message: 'Enter up to 3 letters to include in the SVG:',
       validate: input => input.length <= 3 || 'Please enter a maximum of 3 letters.'
-    },
-    {
-      type: 'select',
-      name: 'shape',
-      message: 'Choose a shape for the SVG:',
-      choices: ['circle', 'triangle', 'rectangle']
     }
   ]);
 }
 
-function generateSVGString(color, letters, shape) {
+function generateSVGString(shape, color, letters) {
   let svgContent;
-
+  
   if (shape === 'circle') {
-    svgContent = `<circle cx="50" cy="50" r="40" fill="${color}" />`;
+    svgContent = `<circle cx="150" cy="100" r="80" fill="${color}"></circle>`;
   } else if (shape === 'triangle') {
-    svgContent = `<polygon points="50,10 90,90 10,90" fill="${color}" />`;
+    svgContent = `<polygon points="160,0 300,200 0,200" fill="${color}"></polygon>`;
   } else {
-    svgContent = `<rect width="100" height="100" fill="${color}" />`;
+    svgContent = `<rect width="300" height="200" fill="${color}"></rect>`;
   }
-  return `<!DOCTYPE html><html><head><title>SVG Logo</title></head><body><svg width="300" height="200">${svgContent}</svg></body></html>`;
+  return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+            ${svgContent}
+            <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">${letters}</text>
+          </svg>`;
 }
 
 function createSVGFile(svgString, filePath) {
   fs.writeFileSync(filePath, svgString);
-  console.log(`SVG file saved at: ${filePath}`);
+  console.log(`Generated logo.svg`);
 }
 
 async function main() {
   const userInput = await getUserInput();
-  const { color, letters } = userInput;
-  const svgString = generateSVGString(color, letters);
-  createSVGFile(svgString, 'logo.html');
+  const { shape, color, letters } = userInput;
+  const svgString = generateSVGString(shape, color, letters);
+  createSVGFile(svgString, 'logo.svg');
 }
 
 main();
-
-
